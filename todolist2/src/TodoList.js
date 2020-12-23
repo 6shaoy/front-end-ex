26e559a9c58079d26bfb4a337a510a4a2b72bee8
6shaoy <6shaoy@gmail.com>
2020-12-24 00:12:06 +0100

    todolist2 = react + redux

---
+++ todolist2/src/TodoList.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+import store from './store';
+
+
+class TodoList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange);
+    }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <Input
+                        value={this.state.inputValue}
+                        placeholder='todo info'
+                        style={{ width: 300, marginRight: 10 }}
+                        onChange={this.handleInputChange}
+                    />
+                    <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
+                </div>
+                <List
+                    style={{marginTop:10, width:300}}
+                    bordered
+                    dataSource={this.state.list}
+                    renderItem={item => (
+                        <List.Item>
+                            {item}
+                        </List.Item>
+                    )}
+                />
+            </div>
+        )
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        }
+        store.dispatch(action);
+    }
+
+    handleStoreChange(){
+        this.setState(store.getState());
+    }
+
+    handleBtnClick(){
+        const action = {
+            type: 'add_todo_item',
+        }
+        store.dispatch(action);
+    }
+}
+
+
+export default TodoList;
