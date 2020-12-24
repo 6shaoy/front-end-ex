03a8c80ff4b613de373ef46e0776ebe7c39913ce
6shaoy <6shaoy@gmail.com>
2020-12-24 22:26:22 +0100

    TodoListUI

--- todolist2/src/TodoList.js
+++ todolist2/src/TodoList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Input, Button, List } from 'antd';
 import store from './store';
 import { getInputChangeAction, getAddItemAction, getItemDeleteAction } from './store/actionCreators';
-
+import TodoListUI from './TodoListUI'
 
 class TodoList extends Component {
 
@@ -13,35 +12,18 @@ class TodoList extends Component {
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleStoreChange = this.handleStoreChange.bind(this);
         this.handleBtnClick = this.handleBtnClick.bind(this);
+        this.handleItemDelete = this.handleItemDelete.bind(this);
         store.subscribe(this.handleStoreChange);
     }
 
     render() {
-        return (
-            <div>
-                <div>
-                    <Input
-                        value={this.state.inputValue}
-                        placeholder='todo info'
-                        style={{ width: 300, marginRight: 10 }}
-                        onChange={this.handleInputChange}
-                    />
-                    <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
-                </div>
-                <List
-                    style={{marginTop:10, width:300}}
-                    bordered
-                    dataSource={this.state.list}
-                    renderItem={(item, index) => (
-                        <List.Item
-                            onClick={this.handleItemDelete.bind(this, index)}
-                        >
-                            {item}
-                        </List.Item>
-                    )}
+        return <TodoListUI 
+                    inputValue={this.state.inputValue}
+                    list={this.state.list}
+                    handleInputChange={this.handleInputChange}
+                    handleBtnClick={this.handleBtnClick}
+                    handleItemDelete={this.handleItemDelete}
                 />
-            </div>
-        )
     }
 
     handleInputChange(e) {
